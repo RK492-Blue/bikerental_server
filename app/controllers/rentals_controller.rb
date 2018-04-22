b3577e1238319f7664c3cde7be286569fe6d0a6c
Winnie Mach <win.horsley5@gmail.com>
2018-04-23 09:42:12 +1000

    merged meena's changes with rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,7 +4,12 @@ class RentalsController < ApplicationController
   # GET /rentals
   # GET /rentals.json
   def index
+
     @rentals = Rental.all
+    if @current_user.present?
+        @rental_user = Rental.where(:user_id => @current_user.id)
+    end
+
   end
 
   # GET /rentals/1
@@ -24,12 +29,12 @@ class RentalsController < ApplicationController
   # POST /rentals
   # POST /rentals.json
   def create
-
     @rental = Rental.new(rental_params)
 
-    # redirect_to @rental
     respond_to do |format|
       if @rental.save
+        Bike.where(:id => @rental.bike_id).update(available: 'f')
+
         format.html { redirect_to @rental, notice: 'Rental was successfully created.' }
         format.json { render :show, status: :created, location: @rental }
       else
@@ -44,6 +49,8 @@ class RentalsController < ApplicationController
   def update
     respond_to do |format|
       if @rental.update(rental_params)
+        Bike.where(:id => @rental.bike_id).update(available: 't',:bikestand_id =>@rental.end_stand_id)
+
         format.html { redirect_to @rental, notice: 'Rental was successfully updated.' }
         format.json { render :show, status: :ok, location: @rental }
       else

0a6c63eff5f4228c3d395f871aba631d4777e8dd
Meena Jim George <meena_jim@yahoo.com>
2018-04-27 09:23:08 +1000

    disabled bikes label on load and when there is no data in the rentals page

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,12 +1,15 @@
 
 $(document).ready(function () {
+  $('.bikelabel').hide().attr('disabled', true);
   $('.bikes').hide().attr('disabled', true);
   $('[name="rental[start_stand_id]"]').on('change', function () {
     const standID = $(this).val();
     $('.bikes').hide().attr('disabled', true);
+    $('.bikelabel').fadeIn().attr('disabled', false);
+
     $('#bikestand_' + standID).fadeIn().attr('disabled', false);
   })
 
-  
+
 
 });
